4d20e8cbbeeb43f63e82a30f473a6999c7c6abfc
ColeFMeldorf <colefmeldorf@gmail.com>
2025-06-08 23:11:56 -0400

    why are our host mass dists different?

--- 1DPolynomialMethod.py
+++ 1DPolynomialMethod.py
@@ -22,6 +22,8 @@ hostgals = fits.open(r"C:\Users\cmeldorf\Downloads\DES-SN5YR_DES_HEAD.FITS\DES-S
 cosmo_sne = pd.read_csv(r"C:\Users\cmeldorf\Downloads\DES-SN5YR_HD+MetaData.csv")[:1635]
 
 hostlib = pd.read_table(r"C:\Users\cmeldorf\Downloads\DES-SN5YR_DES.HOSTLIB\DES-SN5YR_DES.HOSTLIB", comment = '#', skiprows = 57, sep = '\s+')
+#hostlib = pd.read_table(r"C:\Users\cmeldorf\Downloads\corrected_deduped_with_magauto_logsfr_upto_4.HOSTLIB+HOSTNBR\corrected_deduped_with_magauto_logsfr_upto_4.HOSTLIB+HOSTNBR", skiprows = 2, comment = '#', sep = '\s+')
+
 
 mask = contains_mask(hostgals['SNID'], cosmo_sne['CID'].values)
 hostgals = hostgals[mask]
@@ -36,125 +38,92 @@ cosmo_sne['FIELD'] = cosmo_fields
 cosmo_fields[(cosmo_fields == 'C3') | (cosmo_fields == 'X3')] = 1
 cosmo_fields[cosmo_fields !=1] = 0
 
-real_array = np.array([cosmo_sne['zHD'], hostgals['HOSTGAL_MAG_r'], cosmo_sne['c'] - 0.0, cosmo_fields, cosmo_sne['x1']]).T
-print('SUBTRACTING 0.1 FROM C')
-print(real_array.shape)
 
-print(cosmo_sne['zHD'].shape)
+
 print('cutting real data to z < %.2f and z > %.2f' % (MAX_Z, MIN_Z))
-real_array = real_array[(cosmo_sne['zHD'] < MAX_Z) & (cosmo_sne['zHD'] > MIN_Z)]
-real_array.shape
 
-hostgals = hostgals[(cosmo_sne['zHD'] < MAX_Z) & (cosmo_sne['zHD'] > MIN_Z)]
-cosmo_sne = cosmo_sne[(cosmo_sne['zHD'] < MAX_Z) & (cosmo_sne['zHD'] > MIN_Z)]
-hostlib = hostlib[(hostlib['ZTRUE'] < MAX_Z) & (hostlib['ZTRUE'] > MIN_Z)]
+
+cut = (cosmo_sne['zHD'] < MAX_Z) & (cosmo_sne['zHD'] > MIN_Z) & (hostgals['HOSTGAL_MAG_r'] < max_i)
+hostgals = hostgals[cut]
+cosmo_sne = cosmo_sne[cut]
+cosmo_fields = cosmo_fields[cut]
+hostlib = hostlib[(hostlib['ZTRUE'] < MAX_Z) & (hostlib['ZTRUE'] > MIN_Z) & (hostlib['r_obs_auto'] < max_i)]
+
+print(np.shape(hostlib), 'hostlib shape')
+real_array = np.array([cosmo_sne['zHD'], hostgals['HOSTGAL_MAG_r'], cosmo_sne['c'], cosmo_fields, cosmo_sne['x1']]).T
+real_array = real_array[(cosmo_sne['zHD'] < MAX_Z) & (cosmo_sne['zHD'] > MIN_Z)]
+
+plt.subplot(2,3,1)
+loghist, _, _ =plt.hist(hostlib['LOGMASS'], bins = np.linspace(7, 12, 50), histtype = 'step', label = 'Hostlib Masses', color = 'C0', density = True)
+print('Hostlib Masses', np.shape(hostlib['LOGMASS']))
+plt.yscale('log')
+
+x = [7.047789818077926, 7.2102748136431805, 7.353644267876959, 7.444444260682686, 7.568697897942348, 7.731182617864964, 7.912783706046972, 8.156511337216173, 8.357228242372296, 8.562724239593267, 8.744324776489998, 8.94026314086655, 9.169653495841207, 9.422939311140103, 9.657108758179609, 9.881720572374695, 10.163679837492126, 10.46953401164852, 10.75149327676595, 10.937873457012806, 11.066905635052041, 11.200716905156122, 11.339307267325053, 11.420549627286361]
+y = [1132.540765804203, 2143.292994929668, 3018.074830946204, 4965.307582469544, 6270.435393930947, 10808.95843950722, 16197.933716284108, 19828.839972178033, 21102.029679612348, 21102.029679612348, 18061.62933265445, 14526.544191823114, 12241.57508354335, 9693.640935300697, 7918.600975101953, 6468.607034015782, 5366.975513727014, 4665.721857553848, 3471.6886712951996, 2045.5531516076105, 1031.6050532848835, 405.60982495523245, 149.8566984255766, 44.529645577932406]
+
+w_bin_width = np.mean(np.diff(x))
+
+area = np.sum(np.array(y) * w_bin_width)
+
+plt.scatter(x, np.array(y)/area,  color = 'C1', s = 1, label = 'Wiseman et. al. Hostlib Masses')
+
+
+
+plt.legend()
+plt.xlim(8,12)
+
+plt.subplot(2,3,4)
+from scipy import interpolate
+f = interpolate.interp1d(x, y/area, kind='linear', fill_value='extrapolate')
+bins = np.linspace(7, 12, 50)
+bin_centers = (bins[:-1] + bins[1:]) / 2
+plt.plot(bin_centers, loghist - f(bin_centers), label = 'This Work - Wiseman', color = 'C2')
+plt.axhline(0, color = 'C1', linestyle = '--', label = '1')
+
+plt.subplot(2,3,2)
+bins = np.arange(8, 12, 0.25)
+#a, b, _ = plt.hist(hostgals['HOSTGAL_LOGMASS'], density = True, bins = bins)
+
+a,b = np.histogram(hostgals['HOSTGAL_LOGMASS'], density = True, bins = bins)
+b_centers = (b[:-1] + b[1:]) / 2
+plt.plot(b_centers, a, label = 'Hostgal Masses', color = 'C0')
+
+x = [8.127837336112362, 8.371564967281563, 8.643966599554576, 8.902030955633045, 9.145758586802245, 9.379928585127026, 9.633213849140645, 9.876941480309846, 10.130227295608742, 10.397849284531631, 10.632019282856412, 10.85185200498665, 11.13859036216893, 11.367980717143586, 11.630825267857457]
+y = [-0.0016259631403337978, 0.0520325086337578, 0.0650406826850605, 0.10894309425996965, 0.20975609146109098, 0.2520325398956665, 0.31707322258072684, 0.37398380820693883, 0.4422763233869402, 0.5593496084914847, 0.5691057155835301, 0.6357723614089242, 0.31544716565466663, 0.09430895706833332, 0.011382117125242404]
+print(np.size(y))
+plt.plot(x,y,  color = 'C2', label = 'Wiseman et. al.', marker = 'o')
+plt.legend()
+
+plt.subplot(2,3,5)
+plt.plot(b_centers, a - y, label = 'This Work - Wiseman', color = 'C0')
+plt.axhline(0, color = 'C1', linestyle = '--', label = '1')
 
 
 # Getting Field information
 fields = np.array(['C1', 'C2', 'C3', 'X1', 'X2', 'X3', 'S1', 'S2', 'E1', 'E2'])
 field_ras = np.array([54.2743,  54.2743, 52.6484, 34.4757, 35.6645, 36.4500,42.8200, 41.1944, 7.8744 , 9.5000,])
 field_decs = np.array([-27.1116, -29.0884, -28.1000, -4.9295, -6.4121, -4.6000, 0.0000, -0.9884, -43.0096,  -43.9980])  
-#c = SkyCoord(ra=dumpdf['RA'].values*u.degree, dec=dumpdf['DEC'].values*u.degree)
-#catalog = SkyCoord(ra=field_ras*u.degree, dec=field_decs*u.degree)
-#idx, d2d, d3d = c.match_to_catalog_sky(catalog)
-#print(idx)
-#reduced_dump_fields = np.array([fields[i] for i in idx])
-#print(reduced_dump_fields)
-#deepfields = np.where((fields == 'X3') | (fields == 'C3'))[0]
-#deep_bool = np.zeros_like(idx)
-#deep_bool[np.where((idx == deepfields[0]) | (idx == deepfields[1]))[0]] = 1
-
-# c_cuts = [(-0.3, 0.0), (0.0, 0.3)]
-# x_cuts = [(-3, 0), (0,3)]
-
-# bins = np.linspace(0, 1.2, 16)
 
 
-
-# reduced_sim_fields = np.array([i.split('+')[-1] for i in simdf['FIELD']])
-# fields = np.unique(np.array(reduced_sim_fields))
-
-plt.figure(figsize = (5,10))
-
-poly_dict = {}
-
-'''
-for i,f in enumerate(fields):
-  plt.subplot(5,2,i+1)
-  print(f)
-  plt.text(0.5, 0.6, f)
-  for c_cut in c_cuts:
-      for x_cut in x_cuts:
-          simdf_cut = simdf.iloc[np.where(
-                                    (simdf['SIM_c'] > c_cut[0]) 
-                                  & (simdf['SIM_c'] < c_cut[1])
-                                  & (simdf['SIM_x1'] > x_cut[0])
-                                  & (simdf['SIM_x1'] < x_cut[1])
-                                  & (reduced_sim_fields == f))]
-          
-          dumpdf_cut = dumpdf.iloc[np.where(
-                                    (dumpdf['SALT2c'] > c_cut[0]) 
-                                  & (dumpdf['SALT2c'] < c_cut[1])
-                                  & (dumpdf['SALT2x1'] > x_cut[0])
-                                  & (dumpdf['SALT2x1'] < x_cut[1])
-                                  & (reduced_dump_fields == f))]
-          print(np.size(simdf_cut), np.size(dumpdf_cut))
-          dump_counts = np.histogram(dumpdf_cut['GENZ'], bins = bins)[0]
-          sim_counts = np.histogram(simdf_cut['zHD'], bins = bins)[0]
-          bin_centers = bins[:-1]/2 + bins[1:]/2
-
-          w = sim_counts + dump_counts
-
-          error = np.sqrt(1/sim_counts + 1/dump_counts)
-
-          eff = np.nan_to_num(sim_counts/dump_counts)
-          eff[eff>=1 ] = 1
-
-          #plt.errorbar(bin_centers, (sim_counts/dump_counts), yerr = (sim_counts/dump_counts) * error)
-          p = np.poly1d(np.polyfit(bin_centers, eff, deg = 4, w = w))
-
-          
-
-          zz = np.linspace(bin_centers[0], bin_centers[-1],100)
-          plt.plot(zz, p(zz))
-          plt.ylim(0, 1)
-
-          poly_dict[f + '_' + str(c_cut[0]) + '_' + str(x_cut[0])] = p
-          print(eff)
-          print(p)
-
-plt.show()
-'''
 poly_dict = np.load('poly_dict.npy', allow_pickle=True).item()
 print(poly_dict.keys())
 
 ############ Load spec efficiency data ############
 file_path = r"C:\Users\cmeldorf\Desktop\SEARCHEFF_zHOST_DES-SN5YR.DAT"
-#searcheff  = pd.read_csv(file, delimiter='   ', skiprows = 23)
-
 df = pd.DataFrame(columns=['Year', 'Field', 'r_obs_auto', 'obs_gr_auto', 'HOST_EFF'])
 
 with open(file_path, 'r') as file:
     year = 'test'
     field = 'test'
-    for i, line in enumerate(file):
-        
+    for i, line in enumerate(file):   
         if i < 20:
             continue
         line = line.split()
-        #print(line)
-
         if 'Year' in line: 
             year = line[-1]
-            #print(line)
-            #print(year)
         if 'FIELDLIST:' in line:
-            field = line[-1]
-            #print(line)
-            #print(field)
-        
+            field = line[-1]  
         if 'HOSTEFF:' in line:
-            #print(year, field)
             new_rows = pd.DataFrame({'Year': year, 'Field': field, 'r_obs_auto': float(line[1]), 'obs_gr_auto': float(line[2]), 'HOST_EFF': float(line[3])}, index = [0])
             df = pd.concat([df, new_rows], ignore_index=True)
 spec_df =df
@@ -165,7 +134,6 @@ corrections = []
 
 
 for f, c, x1, z, mr in zip(cosmo_sne['FIELD'], cosmo_sne['c'], cosmo_sne['x1'], cosmo_sne['zHD'], hostgals['HOSTGAL_MAG_r']):
-    print(f,c,x1,z)
     f_reduced = f.split('+')[-1]
     c_index = -0.3 if c < 0 else 0.0
     x_index = -3 if x1 < 0 else 0
@@ -231,19 +199,13 @@ Vmax, Vmaxbins, Vmax_correction, counts = calc_Vmax(max_z = MAX_Z, max_i = max_i
 N_SN_no_corr = np.histogram(cosmo_sne['HOST_LOGMASS'], bins = bins)
 bin_centers = (bins[:-1] + bins[1:]) / 2
 
-
+plt.subplot(2,3,3)
 N_SN = scipy.stats.binned_statistic(cosmo_sne['HOST_LOGMASS'], correction * volume_factor,  bins = bins, statistic = 'sum')[0] 
 
-
-
-plt.plot(bin_centers, ((N_SN / Vmax) * 1/total_time), label = 'Inferred SNe', color = 'C1')
-
-# corrected_N_SN = ((N_SN / Vmax) * 1/total_time) * mass_correction
-# plt.plot(bin_centers, corrected_N_SN, label = 'Inferred SNe', color = 'C3')
+plt.plot(bin_centers[1:], ((N_SN / Vmax) * 1/total_time)[1:], label = 'Inferred SNe', color = 'C1')
 
 
 plt.yscale('log')
-#plt.ylim(1e-4, 10**(-1))
 x = [8.366934932446702, 8.620967291793225, 8.874999651139749, 9.116935630167202, 9.370968687234226, 9.62499965113975, 9.872984866907489, 10.127015830813011, 10.387096729179323, 10.63508054950606, 10.883064369832798, 11.125000348860251, 11.372984169186989, 11.645161447872372]
 y = [-3.5696322907036198, -3.511605428268646, -3.3326885718235055, -3.163442859117528, -2.9361702839765758, -2.7669245712705974, -2.6460348561686864, -2.457446855984384, -2.259187712060919, -2.147968861789915, -1.9690522842530296, -1.891682576523221, -1.9158605753252544, -2.0125725705333872]
 x = np.array(x)
--- ColeUtils.py
+++ ColeUtils.py
@@ -5,6 +5,7 @@ from astropy.io import fits
 from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
 import scipy.stats
+
 def contains_mask(list1, list2):
     return [element in list2 for element in list1]
 
@@ -39,8 +40,6 @@ def rate_error(a,b):
     tot_err = np.sqrt((a_frac**2) + (b_frac**2)) * (a / b)
     return tot_err
 
-def calculate_Vmax_correction(hostlib, max_z, min_z, max_i):
-    cosmo = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Tcmb0=2.725 * u.K, Om0=0.3)
 
 def calculate_volume(minz, maxz, area):
     cosmo = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Tcmb0=2.725 * u.K, Om0=0.3)
@@ -54,3 +53,81 @@ def calculate_volume(minz, maxz, area):
     V = survey_volume / 41252.96125
     return V
 
+def calc_Vmax(max_z, max_i, measured_z, measured_i, mass, bins, min_z = 0):
+    cosmo = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Tcmb0=2.725 * u.K, Om0=0.3)
+    cut = (measured_i < max_i) & (measured_z < max_z) & (measured_z > min_z) 
+    measured_z = measured_z[cut]
+    measured_i = measured_i[cut]
+    mass = mass[cut]
+    print('Z cutoff', max_z)
+    print('Largest z in measured_z', np.max(measured_z))
+    measured_z = np.asarray(measured_z)
+    measured_i = np.asarray(measured_i)
+    rsurv_gal = cosmo.luminosity_distance(max_z).value
+    print('Luminosity distance at max z', rsurv_gal)
+    robs = cosmo.luminosity_distance(measured_z).value
+    print('Luminosity distance at measured z', robs[:10])
+    print('measured_i', measured_i[:10])
+    mobs = measured_i
+    msurv = max_i
+    distance_modulus = 10 ** ((msurv - mobs)/5) 
+    rmax = robs * distance_modulus
+    print('rmax', rmax[:10])
+    print(robs/rmax)
+    Vmax_correction = np.empty_like(rmax)
+    Vmax_correction = (rsurv_gal / rmax)**3
+    Vmax_correction[Vmax_correction < 1] = 1
+    print('inf redshifts:')
+    print(measured_z[np.where(Vmax_correction == np.inf)])
+    print('and robs')
+    print(robs[np.where(Vmax_correction == np.inf)])
+    print('>1 fraction', np.size(Vmax_correction[np.where(Vmax_correction > 1)]) / np.size(Vmax_correction))
+    print('max and min', np.max(Vmax_correction), np.min(Vmax_correction))
+
+    Vmax_binned, bin_edges = scipy.stats.binned_statistic(mass, Vmax_correction, bins=bins, statistic='sum')[:2]
+    counts, bin_edges = scipy.stats.binned_statistic(mass, Vmax_correction, bins=bins, statistic='count')[:2]
+    print('mean vmax correction', np.mean(Vmax_correction))
+    print(Vmax_binned/counts)
+
+
+    return Vmax_binned, bin_edges, Vmax_correction, counts
+
+from matplotlib import rcParams
+import matplotlib as mpl
+def update_rcParams(key, val):
+    if key in rcParams:
+        rcParams[key] = val
+
+def LaurenNicePlots():
+    update_rcParams('font.size', 10)
+    update_rcParams('font.family', 'serif')
+    update_rcParams('xtick.major.size', 8)
+    update_rcParams('xtick.labelsize', 'large')
+    update_rcParams('xtick.direction', "in")
+    update_rcParams('xtick.minor.visible', True)
+    update_rcParams('xtick.top', True)
+    update_rcParams('ytick.major.size', 8)
+    update_rcParams('ytick.labelsize', 'large')
+    update_rcParams('ytick.direction', "in")
+    update_rcParams('ytick.minor.visible', True)
+    update_rcParams('ytick.right', True)
+    update_rcParams('xtick.minor.size', 4)
+    update_rcParams('ytick.minor.size', 4)
+    update_rcParams('xtick.major.pad', 10)
+    update_rcParams('ytick.major.pad', 10)
+    update_rcParams('legend.numpoints', 1)
+    update_rcParams('mathtext.fontset', 'cm')
+    update_rcParams('mathtext.rm', 'serif')
+    update_rcParams('axes.labelsize', 'x-large')
+    update_rcParams('lines.marker', 'None')
+    update_rcParams('lines.markersize', 1)
+    update_rcParams('lines.markeredgewidth', 1.0)
+    update_rcParams('lines.markeredgecolor', 'auto')
+
+    cycle_colors = ['navy', 'maroon','darkorange', 'darkorchid', 'darkturquoise', 'darkmagenta', '6FADFA','7D7D7D','black']
+    # cycle_colors = ['9F6CE6','FF984A','538050','6FADFA','7D7D7D','black']
+    cycle_markers = ['o','^','*','s','X','d', '1','2', '3']
+    # cycle_colors = ['darkorchid','darkorange','darkturquoise']
+    # cycle_markers = ['o','^','*']
+    #+ mpl.cycler(marker=cycle_markers)
+    update_rcParams('axes.prop_cycle', mpl.cycler(color=cycle_colors) )
